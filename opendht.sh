6320802dd908e67891bfc5b012cbfe117445f2f8
ivan-f-n <82468157+ivan-f-n@users.noreply.github.com>
2021-03-29 13:52:53 +0200

    Added Fusepp C++11 wrapper of Fuse library

--- opendht.sh
+++ opendht.sh
@@ -3,6 +3,6 @@ set -ex
 git clone https://github.com/savoirfairelinux/opendht.git
 cd opendht
 mkdir build && cd build
-cmake -DOPENDHT_PYTHON=ON -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DOPENDHT_PEER_DISCOVERY=OFF -DOPENDHT_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j4
 sudo make install
