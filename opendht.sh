b2896f2d1038d37e546e3f0a6460b99c8a8291c2
ivan-f-n <82468157+ivan-f-n@users.noreply.github.com>
2021-04-01 13:56:33 +0200

    Continuous integration changes

--- opendht.sh
+++ opendht.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -ex
+wget https://github.com/libfuse/libfuse/releases/download/fuse-3.9.0/fuse-3.9.0.tar.xz
+tar -xf fuse-3.9.0.tar.xz
+cd fuse-3.9.0/
+mkdir build &&
+cd    build &&
+meson --prefix=/usr .. &&
+ninja
+sudo ninja install
 git clone https://github.com/savoirfairelinux/opendht.git
 cd opendht
 mkdir build && cd build
